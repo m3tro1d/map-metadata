6156393f0951f63700ae46ed5a14a9e16f72ed84
m3tro1d <qazwf734@gmail.com>
2020-07-04 21:15:03 +0300

    Initial commit

---
+++ map-metadata.py
@@ -0,0 +1,11 @@
+import os
+import re
+import suprocess
+import sys
+
+# Form a regex for files
+
+# Parse input parameters
+
+# Loop through folders contents
+	# Match file regex and process the file
