e64f5179124e97dff29bb68652ccabac137aa4d9
m3tro1d <qazwf734@gmail.com>
2020-07-04 21:16:20 +0300

    Add song regexp

--- map-metadata.py
+++ map-metadata.py
@@ -3,7 +3,9 @@ import re
 import suprocess
 import sys
 
-# Form a regex for files
+
+# Regexp for files in format "artist - title.mp3"
+song_regex = re.compile(r'(.+) - (.+)\.mp3')
 
 # Parse input parameters
 
