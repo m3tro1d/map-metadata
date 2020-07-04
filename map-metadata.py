6714d4cd46cd29be1dd845a435b3fd60c62c7b62
m3tro1d <qazwf734@gmail.com>
2020-07-04 21:21:23 +0300

    Add input & output parameters

--- map-metadata.py
+++ map-metadata.py
@@ -14,7 +14,15 @@ parser = argparse.ArgumentParser(
 	description="""This script maps metadata from files' names, so you don't
 have to enter it manually.""")
 
+parser.add_argument("input_dir",
+	help="directory to grab mp3 files from")
+
+parser.add_argument("output_dir",
+	help="directory to place processed files to")
+
 args = parser.parse_args()
+input_dir = args.input_dir
+output_dir = args.output_dir
 
 # Loop through folders contents
 	# Match file regex and process the file
