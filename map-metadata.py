6a2fbc065559e97ab38e6f1f3ca60b4ab3b487a4
m3tro1d <qazwf734@gmail.com>
2020-07-04 21:18:53 +0300

    Add argument parser

--- map-metadata.py
+++ map-metadata.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import re
 import suprocess
@@ -7,7 +8,13 @@ import sys
 # Regexp for files in format "artist - title.mp3"
 song_regex = re.compile(r'(.+) - (.+)\.mp3')
 
+
 # Parse input parameters
+parser = argparse.ArgumentParser(
+	description="""This script maps metadata from files' names, so you don't
+have to enter it manually.""")
+
+args = parser.parse_args()
 
 # Loop through folders contents
 	# Match file regex and process the file
