8ef089af0c3fb695cb1fd515b6c6ff04c5c853f6
m3tro1d <qazwf734@gmail.com>
2020-09-30 20:06:46 +0300

    Put everything in main()

--- map-metadata.py
+++ map-metadata.py
@@ -13,49 +13,56 @@ def get_artists_and_title(match):
     return (artists, title)
 
 
-# Regexp for files in format "artist - title.mp3"
-song_regex = re.compile(r'(.+) - (.+)\.mp3')
+def main():
+    """Entry point of the script"""
+    # Regexp for files in format "artist - title.mp3"
+    song_regex = re.compile(r'(.+) - (.+)\.mp3')
 
 
-# Parse input parameters
-parser = argparse.ArgumentParser(
-    description="""This script maps metadata from files' names, so you don't
-    have to enter it manually.""")
+    # Parse input parameters
+    parser = argparse.ArgumentParser(
+        description="""This script maps metadata from files' names, so you don't
+        have to enter it manually.""")
 
-parser.add_argument("input_dir",
-                    help="directory to grab mp3 files from")
+    parser.add_argument("input_dir",
+                        help="directory to grab mp3 files from")
 
-parser.add_argument("output_dir",
-                    help="directory to place processed files to")
+    parser.add_argument("output_dir",
+                        help="directory to place processed files to")
 
-args = parser.parse_args()
-input_dir = os.path.abspath(args.input_dir)
-output_dir = os.path.abspath(args.output_dir)
-
-
-# Process input directories
-# Check input directory
-if not os.path.exists(input_dir):
-    print("Directory '{}' does not exist, exiting.".format(input_dir))
-    sys.exit(1)
-# Check output directory
-if not os.path.exists(output_dir):
-    print("Creating '{}'...".format(output_dir))
-    os.mkdir(output_dir)
+    args = parser.parse_args()
+    input_dir = os.path.abspath(args.input_dir)
+    output_dir = os.path.abspath(args.output_dir)
 
 
-# Loop through input folder's contents
-for song_name in os.listdir(input_dir):
-    # Find matches and process them
-    match = song_regex.match(song_name)
-    if match:
-        artists, title = get_artists_and_title(match)
-        # Process the file
-        subprocess.run([
-            'ffmpeg', '-y',
-            '-i', os.path.join(input_dir, song_name),
-            '-metadata', 'artist={}'.format(artists),
-            '-metadata', 'title={}'.format(title),
-            '-c', 'copy',
-            os.path.join(output_dir, song_name)
-        ])
+    # Process input directories
+    # Check input directory
+    if not os.path.exists(input_dir):
+        print("Directory '{}' does not exist, exiting.".format(input_dir))
+        sys.exit(1)
+    # Check output directory
+    if not os.path.exists(output_dir):
+        print("Creating '{}'...".format(output_dir))
+        os.mkdir(output_dir)
+
+
+    # Loop through input folder's contents
+    for song_name in os.listdir(input_dir):
+        # Find matches and process them
+        match = song_regex.match(song_name)
+        if match:
+            artists, title = get_artists_and_title(match)
+            # Process the file
+            subprocess.run([
+                'ffmpeg', '-y',
+                '-i', os.path.join(input_dir, song_name),
+                '-metadata', 'artist={}'.format(artists),
+                '-metadata', 'title={}'.format(title),
+                '-c', 'copy',
+                os.path.join(output_dir, song_name)
+            ])
+
+
+# Entry point
+if __name__ == "__main__":
+    main()
