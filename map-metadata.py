b6aa86037d3fd389bc957e2609eaf8931f0cfcfd
m3tro1d <qazwf734@gmail.com>
2020-09-30 20:11:38 +0300

    Refactor a lot of things

--- map-metadata.py
+++ map-metadata.py
@@ -5,6 +5,10 @@ import subprocess
 import sys
 
 
+# Regexp for files in format "artist - title.mp3"
+SONG_REGEX = re.compile(r'(.+) - (.+)\.mp3')
+
+
 def get_artists_and_title(match):
     """Returns a tuple of artists and title in specified name"""
     # This will correctly display multiple artists in windows
@@ -13,29 +17,20 @@ def get_artists_and_title(match):
     return (artists, title)
 
 
-def main():
-    """Entry point of the script"""
-    # Regexp for files in format "artist - title.mp3"
-    song_regex = re.compile(r'(.+) - (.+)\.mp3')
-
-
-    # Parse input parameters
+def parse_arguments():
+    """Processes the arguments"""
     parser = argparse.ArgumentParser(
         description="""This script maps metadata from files' names, so you don't
         have to enter it manually.""")
-
     parser.add_argument("input_dir",
                         help="directory to grab mp3 files from")
-
     parser.add_argument("output_dir",
                         help="directory to place processed files to")
-
-    args = parser.parse_args()
-    input_dir = os.path.abspath(args.input_dir)
-    output_dir = os.path.abspath(args.output_dir)
+    return parser.parse_args()
 
 
-    # Process input directories
+def check_dirs(input_dir, output_dir):
+    """Checks if the dirs are presented & creates output_dir if necessary"""
     # Check input directory
     if not os.path.exists(input_dir):
         print("Directory '{}' does not exist, exiting.".format(input_dir))
@@ -46,10 +41,12 @@ def main():
         os.mkdir(output_dir)
 
 
+def process_files(input_dir, output_dir):
+    """Processes the files"""
     # Loop through input folder's contents
     for song_name in os.listdir(input_dir):
         # Find matches and process them
-        match = song_regex.match(song_name)
+        match = SONG_REGEX.match(song_name)
         if match:
             artists, title = get_artists_and_title(match)
             # Process the file
@@ -62,6 +59,19 @@ def main():
                 os.path.join(output_dir, song_name)
             ])
 
+
+def main():
+    """Entry point of the script"""
+    # Get the input arguments
+    args = parse_arguments()
+    input_dir = os.path.realpath(args.input_dir)
+    output_dir = os.path.realpath(args.output_dir)
+
+    # Check the directories
+    check_dirs(input_dir, output_dir)
+
+    # Process the songs
+    process_files(input_dir, output_dir)
 
 # Entry point
 if __name__ == "__main__":
